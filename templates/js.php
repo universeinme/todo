50c4cacef96242a8b0abda44a778705f1d0b3d04
Eko Priambodo <universe.inm@gmail.com>
2023-05-13 20:47:27 +0700

    Update js.php
    Mungkin pake axios buat narik data ke db via ajax request

--- templates/js.php
+++ templates/js.php
@@ -1,4 +1,5 @@
 <?php ?>
 <script src="https://cdn.tailwindcss.com"></script>
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios@1.4.0/dist/axios.min.js"></script>
 <script src="./assets/js/script.js"></script>
