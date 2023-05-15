aedd96d14f39af9105e15ac4fcafbb1f80a2778d
Eko Priambodo <universe.inm@gmail.com>
2023-05-15 13:37:25 +0700

    Update script.js
    Edit Modal Popup and Update button is working now!~

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 function isDialogOpen() {
-    return {
-        modal: false,
-        open() {
-            this.modal = true;
-            document.body.classList.add("modal-open");
-        },
-        close() {
-            this.modal = false;
-            document.body.classList.remove("modal-open");
-        },
-        isOpen() {
-            return this.modal === true
-        },
-    }
+	return {
+		modal: false,
+		open() {
+			this.modal = true;
+			document.body.classList.add("modal-open");
+		},
+		close() {
+			this.modal = false;
+			document.body.classList.remove("modal-open");
+		},
+		isOpen() {
+			return this.modal === true
+		},
+	}
 }
 
 function reassignDialog() {
-    return {
-        reassignmodal: false,
-        openReassign() {
-            this.reassignmodal = true;
-            document.body.classList.add("modal-open");
-        },
-        closeReassign() {
-            this.reassignmodal = false;
-            document.body.classList.remove("modal-open");
-        },
-        isOpenReassign() {
-            return this.reassignmodal === true
-        },
-    }
+	return {
+		reassignmodal: false,
+		openReassign() {
+			this.reassignmodal = true;
+			document.body.classList.add("modal-open");
+		},
+		closeReassign() {
+			this.reassignmodal = false;
+			document.body.classList.remove("modal-open");
+		},
+		isOpenReassign() {
+			return this.reassignmodal === true
+		},
+	}
 }
 
 function createDialog() {
-    return {
-        createModal: false,
-        openCreate() {
-            this.createModal = true;
-            document.body.classList.add("modal-open");
-        },
-        closeCreateModal() {
-            this.createModal = false;
-            document.body.classList.remove("modal-open");
-        },
-        isOpenCreateModal() {
-            return this.createModal === true
-        },
-    }
+	return {
+		createModal: false,
+		openCreate() {
+			this.createModal = true;
+			document.body.classList.add("modal-open");
+		},
+		closeCreateModal() {
+			this.createModal = false;
+			document.body.classList.remove("modal-open");
+		},
+		isOpenCreateModal() {
+			return this.createModal === true
+		},
+	}
 }
 
 /*function editDialog() {
@@ -127,49 +127,75 @@ function createDialog() {
 }*/
 
 function hapusTask(id) {
-    if (confirm("Are you sure you want to delete this task?")) {
-        // send AJAX request to delete task from database
-        fetch('./funcs/delete.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: 'id=' + id
-        })
-            .then(response => {
-                if (response.ok) {
+	if (confirm("Are you sure you want to delete this task?")) {
+		// send AJAX request to delete task from database
+		fetch('./funcs/delete.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: 'id=' + id
+		})
+			.then(response => {
+				if (response.ok) {
 
-                    // remove task from view if server confirms deleteion
-                    var taskCard = document.getElementById('task-' + id);
-                    taskCard.parentNode.removeChild(taskCard);
+					// remove task from view if server confirms deleteion
+					var taskCard = document.getElementById('task-' + id);
+					taskCard.parentNode.removeChild(taskCard);
 
-                    // display success message
-                    alert("Task successfully deleted!");
-                }
-            })
-            .catch(error => console.error(error));
-    }
+					// display success message
+					alert("Task successfully deleted!");
+				}
+			})
+			.catch(error => console.error(error));
+	}
 }
 
 function editTask(id) {
-    // Get the task data from the server using AJAX
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            // Parse the JSON response
-            var task = JSON.parse(this.responseText);
 
-            // Populate the modal form with the task data
-            document.getElementById('task-id').value = task.id;
-            document.getElementById('task-title').value = task.judul;
-            document.getElementById('task-description').value = task.deskripsi;
-            document.getElementById('task-date').value = task.tgl_tempo;
+	// Get the task data from the server using AJAX
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+			// Parse the JSON response
+			var task = JSON.parse(this.responseText);
 
-            // Show the modal dialog
-            var modal = document.getElementById('edit-modal');
-            modal.style.display = 'block';
-        }
-    };
-    xhr.open('GET', './funcs/pilih_task.php?id=' + id, true);
-    xhr.send();
+			// Populate the modal form with the task data
+			document.getElementById('task-id').value = task.id;
+			document.getElementById('task-title').value = task.judul;
+			document.getElementById('task-description').value = task.deskripsi;
+			document.getElementById('task-date').value = task.tgl_tempo;
+
+		}
+	};
+	xhr.open('GET', './funcs/pilih_task.php?id=' + id, true);
+	xhr.send();
 }
+
+// Show the modal dialog
+var modal = document.getElementById('edit-modal');
+
+// Open modal
+var openModal = document.querySelector('#open-modal');
+openModal.addEventListener('click', () => {
+	modal.classList.remove('hidden');
+});
+
+// Close modal
+const closeModal = document.getElementById('close-modal');
+closeModal.addEventListener('click', () => {
+	modal.classList.add('hidden');
+});
+
+// Cancel modal
+const cancelModal = document.getElementById('cancel-modal');
+cancelModal.addEventListener('click', () => {
+	modal.classList.add('hidden');
+});
+
+// Close modal on overlay click
+modal.addEventListener('click', (e) => {
+	if (e.target === modal) {
+		modal.classList.add('hidden');
+	}
+});
