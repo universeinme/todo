e29a1593d16223cf1bd2bfceed69a6d70114728d
Eko Priambodo <universe.inm@gmail.com>
2023-05-16 11:01:26 +0700

    Update todo_app.sql

--- todo_app.sql
+++ todo_app.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.11
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 29, 2023 at 11:21 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Host: localhost
+-- Generation Time: May 16, 2023 at 04:00 AM
+-- Server version: 5.7.11
+-- PHP Version: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,20 +30,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `judul` text NOT NULL,
   `deskripsi` text NOT NULL,
   `tgl_tempo` date NOT NULL,
   `status` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `judul`, `deskripsi`, `tgl_tempo`, `status`) VALUES
-(5, 'Judul Todo', 'Isi deskripsi todo', '2023-05-17', 0),
-(5, 'Judul #2', 'Deskripsi #2', '2023-05-27', 0),
-(6, 'Judul #3', 'Deskripsi #3', '2023-05-31', 0);
+INSERT INTO `tasks` (`id`, `user_id`, `judul`, `deskripsi`, `tgl_tempo`, `status`) VALUES
+(1, 5, 'Judul Todo', 'Isi deskripsi todo', '2023-05-17', 0),
+(2, 5, 'Judul #2', 'Deskripsi #2', '2023-05-27', 0),
+(3, 6, 'Judul #5', 'Deskripsi #5', '2023-05-30', 0),
+(4, 7, 'Test #5 Edit2', 'Test #5 Edit2', '2023-05-22', 1),
+(12, 5, 'Test Empat', 'Test Empat', '2023-05-30', 0),
+(13, 5, 'Test Empat', 'Test Empat', '2023-05-30', 0),
+(17, 7, 'Test #5 Edit 2', 'Test #5 Edit 2', '2023-05-22', 1),
+(18, 6, 'Test #5 Edit00', 'Test #5 Edit00', '2023-06-01', 1);
 
 -- --------------------------------------------------------
 
@@ -55,7 +62,7 @@ CREATE TABLE `users` (
   `username` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
@@ -63,7 +70,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 (5, 'tiga', 'tiga@gmail.com', '0ac0e80f3d604b1f9fd60a42645edc16'),
-(6, 'empat', 'empat@gmail.com', '0ac0e80f3d604b1f9fd60a42645edc16');
+(6, 'empat', 'empat@gmail.com', '0ac0e80f3d604b1f9fd60a42645edc16'),
+(7, 'tujuh', 'tujuh@gmail.com', '0ac0e80f3d604b1f9fd60a42645edc16'),
+(8, 'delapan', 'delapan@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
 
 --
 -- Indexes for dumped tables
@@ -73,7 +82,8 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 -- Indexes for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD KEY `owner` (`id`);
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD KEY `owner` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -87,10 +97,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
@@ -100,7 +116,7 @@ ALTER TABLE `users`
 -- Constraints for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `owner` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `owner` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
